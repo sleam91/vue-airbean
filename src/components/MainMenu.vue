f0f8e4cde523cf7e1eb64ec83691df20a0ebee9d
Ivan Jurkovic <icasamoubica@yahoo.co.uk>
2020-05-27 14:42:13 +0200

    made basic layout for landing and main menu

---
+++ src/components/MainMenu.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="mainMenu">
+      <button class="exit">
+      </button>
+      <h3>Meny</h3>
+      <p>___________________</p>
+      <h3>Vårt kaffe</h3>
+      <p>___________________</p>
+      <h3>Min profil</h3>
+      <p>___________________</p>
+      <h3>Orderstatus</h3>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scope lang="scss">
+.mainMenu {
+    z-index: 10;
+    position: absolute;
+    color: white;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: #2F2926;
+    h3 {
+        font-size: 2rem;
+    }
+    p {
+        font-size: 0.1rem;
+    }
+    p, h3 {
+        margin: 0.3rem;
+    }
+
+}
+.exit {
+    height: 2.5rem;
+    width: 2.5rem;
+    background-image: url('../assets/graphics/close.svg');
+    background-size: 1.1rem;
+    background-repeat: no-repeat;
+    background-position: center;
+    padding: none;
+    margin: none;
+    box-shadow: none;
+    position: absolute;
+    left: 1rem;
+    top: 1rem;
+    background-color: white;
+    border: none;
+    border-radius: 50%;
+}
+</style>
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,18 +1,43 @@
 <template>
-
   <div class="landing">
+    <main-menu/>
+    <div class="left">
+      <img src="@/assets/graphics/intro-graphic-left.svg" alt="">
+    </div>
+    <div class="center">
+      <img src="@/assets/graphics/airbean-landing.svg" alt="">
+    </div>
+   <div class="right">
+     <img src="@/assets/graphics/intro-graphic-right.svg" alt="">
+   </div>
   </div>
-
 </template>
 
 <script>
+import MainMenu from '@/components/MainMenu'
 export default {
   name: 'Landing',
-
-  
+  components: {
+    MainMenu
+  }
 }
 </script>
 
 <style scoped lang="scss">
-
+.landing {
+  display: flex;
+  justify-content: space-between;
+  background-color: rgb(69, 87, 87);
+}
+.center {
+  position: absolute;
+  z-index: 2;
+  left: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.no-display {
+  display: none;
+}
 </style>
