62343b54924e28a7cdbf5ba846f55c852251aa0e
Sebastian Alvarez <sleam91@gmail.com>
2020-05-28 08:55:22 +0200

    done login form with input check

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,101 +3,49 @@
         <img :src="require('@/assets/graphics/logo.svg')" alt="icon" />
         <h1>{{title}}</h1>
         <h3>{{subtitle}}</h3>
-        <form action>
-            <p class="input-name">
-                <label for="name">Namn</label>
-                <input id="name" v-model="name" type="text" name="name" />
-            </p>
-            <p class="input-email">
-                <label for="email">Epost</label>
-                <input id="email" v-model="email" type="text" name="email" />
-            </p>
-            <p class="GDPR-checkbox">
-            <input type = "checkbox"
-                 id = "GDPR"
-                 value = "GDPR" />
-             <label for = "GDPR">GDPR Ok!</label>
-             </p>
-            <p class="submit-button">
-                <input type="submit" value="Brew me a cup!" />
-            </p>
-        </form>
+       <login-form/>
     </div>
 </template>
 
 <script>
+import LoginForm from "@/components/LoginForm";
 export default {
     name: "Login",
+    components: {
+        LoginForm
+    },
     data() {
         return {
             title: "Välkommen till AirBean-familjen!",
             subtitle:
                 "Genom att skapa ett konto nedan kan du spara och se din orderhistorik.",
-            name: "",
-            email: ""
+
         };
-    }
+    },
 };
 </script>
 
 <style lang="scss" scoped>
 .login {
     background: #f3e4e1;
-    padding: 2rem 1rem;
-    margin: 5rem 1rem 0;
+    padding: 2rem 2rem;
+    margin: 3rem 1rem 0;
     max-width: 480px;
     display: flex;
     flex-direction: column;
-    align-items: center;
+    justify-content: center;
     border: 1px solid black;
     border-radius: 5px;
     text-align: center;
     font-family: "Work Sans";
-}
-
-.input-name,
-.input-email {
-    padding-left: 1rem;
-    padding-right: 1rem;
-    text-align: start;
-    display: flex;
-    flex-direction: column;
-    font-size: 0.75rem;
-    
-    input {
-        background: #f3e4e1;
-        font-family: "Work Sans";
-        border-color: black;
-        font-size: large;
-        border-width: 1px;
-        border-radius: 6px;
-        height: 3rem;
-        padding: 0 1rem;
-        min-width: 70vw;;
+    color: #2f2926;
+    h1 {
+        font-family: "PT Serif";
+    }
+    h3 {
+        font-weight: normal;
     }
 }
 
-h1 {
-    font-family: "PT Serif";
-}
-h3 {
-    font-weight: normal;
-}
-input[type="submit" i] {
-    padding: 0 2.5rem;
-    height: 3.6rem;
-    font-size: 1.5rem;
-    font-family: "PT Serif";
-    border-radius: 1.8rem;
-    color: #ffffff;
-    border: none;
-    background: #2f2926;
-    letter-spacing: 1.2px;
-    vertical-align: middle;
-}
-.GDPR-checkbox{
-    display: flex;
-    padding: 0 1rem;
-    font-size: 0.75rem;
-}
+
 </style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,31 +1,37 @@
 <template>
-
-  <div class="profile">
-    <login v-if="loggedOut"/>
-
-  </div>
-
+    <div class="profile">
+        <login v-if="!$store.state.loggedIn" />
+        <order-history v-else/>
+    </div>
 </template>
 
 <script>
-import Login from '@/components/Login'
+import Login from "@/components/Login";
+import OrderHistory from "@/components/OrderHistory";
 export default {
-  name: 'Profile',
-  components:{
-    Login
-  },
-  data(){
-    
-      return{
-        loggedOut:true
-      }
-    
-  }
+    name: "Profile",
+    components: {
+        Login,
+        OrderHistory
+    },
+    data() {
+        return {
+            
+        };
+    },
 
-  
-}
+};
 </script>
 
 <style scoped lang="scss">
+
+.profile {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    background-color: #2f2926;
+}
 
 </style>
