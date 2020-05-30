5bd68b851aeac6898cd71e0e761d364776eb7fbe
Sebastian Alvarez <sleam91@gmail.com>
2020-05-31 00:26:26 +0200

    save to session storage when adding item

--- src/components/CoffeeMenuItem.vue
+++ src/components/CoffeeMenuItem.vue
@@ -24,6 +24,7 @@ export default {
     methods: {
         addItem(menuItem) {
             this.$store.commit('addItemToBasket', menuItem)
+            sessionStorage.setItem("storeState", JSON.stringify(this.$store.state));
         }
     }
 }
