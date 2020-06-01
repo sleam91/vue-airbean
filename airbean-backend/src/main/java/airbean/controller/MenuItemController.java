9aebd8534ffabb2312e006eb9d53d285c21c7f1f
Sebastian Alvarez <sleam91@gmail.com>
2020-06-01 10:24:08 +0200

    removed CrossOrigins annotation

--- airbean-backend/src/main/java/airbean/controller/MenuItemController.java
+++ airbean-backend/src/main/java/airbean/controller/MenuItemController.java
@@ -3,7 +3,6 @@ package airbean.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,22 +12,18 @@ import airbean.model.MenuItem;
 
 @RestController
 public class MenuItemController {
-	
+
 	@Autowired
 	private MenuItemService menuItemService;
-	
-	@CrossOrigin(origins = "http://localhost:8080")
+
 	@RequestMapping("/api/menuitems")
 	public List<MenuItem> getAllItems() {
 		return menuItemService.getAllItems();
 	}
-	
-	
-	@CrossOrigin(origins = "http://localhost:8080")
+
 	@PostMapping("/api/menuitems")
 	public void addItem(@RequestBody MenuItem menuItem) {
 		menuItemService.addItem(menuItem);
 	}
 
-	
 }
--- airbean-backend/src/main/java/airbean/controller/OrderController.java
+++ airbean-backend/src/main/java/airbean/controller/OrderController.java
@@ -1,7 +1,6 @@
 package airbean.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,13 +14,11 @@ public class OrderController {
 	@Autowired
 	private OrderService orderService;
 
-	@CrossOrigin(origins = "http://localhost:8080")
 	@RequestMapping("/api/orders/highest")
 	public int getHighestOrderNo() {
 		return orderService.getHighestOrderNo();
 	}
-	
-	@CrossOrigin(origins = "http://localhost:8080")
+
 	@PostMapping("/api/orders")
 	public void addOrderNoUser(@RequestBody Order order) {
 		orderService.addOrderNoUser(order);
